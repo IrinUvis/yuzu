73b02cda4fa333100fb26112d4933e18df34071d
IrinUvis <karol.kowalski0121@gmail.com>
2025-03-24 22:12:13 +0100

    Add Language model and LanguageOptions composable with English and Polish options.

---
+++ feature/wordgenerator/src/main/java/uvis/irin/feature/wordgenerator/ui/components/PartsOfSpeechOptions.kt
@@ -0,0 +1,103 @@
+package uvis.irin.feature.wordgenerator.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import uvis.irin.core.designsystem.components.checkbox.YuzuCheckboxOption
+import uvis.irin.core.designsystem.components.section.SectionHeader
+import uvis.irin.core.designsystem.components.text.HeaderWithSubtitle
+import uvis.irin.core.designsystem.preview.YuzuPreview
+import uvis.irin.feature.wordgenerator.ui.model.PartOfSpeech
+
+@Composable
+fun PartsOfSpeechOptions(
+    modifier: Modifier = Modifier,
+    selectedPartsOfSpeech: Set<PartOfSpeech>,
+    onClick: (PartOfSpeech) -> Unit,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        SectionHeader(text = "Parts of speech")
+        PartsOfSpeechCheckboxGroup(
+            selectedPartsOfSpeech = selectedPartsOfSpeech,
+            onClick = onClick,
+        )
+    }
+}
+
+@Composable
+private fun PartsOfSpeechCheckboxGroup(
+    modifier: Modifier = Modifier,
+    selectedPartsOfSpeech: Set<PartOfSpeech>,
+    onClick: (PartOfSpeech) -> Unit,
+) {
+    Column(modifier = modifier) {
+        PartOfSpeechCheckbox(
+            partOfSpeech = PartOfSpeech.Noun,
+            checked = PartOfSpeech.Noun in selectedPartsOfSpeech,
+            onClick = onClick,
+        )
+        PartOfSpeechCheckbox(
+            partOfSpeech = PartOfSpeech.Verb,
+            checked = PartOfSpeech.Verb in selectedPartsOfSpeech,
+            onClick = onClick,
+        )
+        PartOfSpeechCheckbox(
+            partOfSpeech = PartOfSpeech.Adjective,
+            checked = PartOfSpeech.Adjective in selectedPartsOfSpeech,
+            onClick = onClick,
+        )
+    }
+}
+
+@Composable
+private fun PartOfSpeechCheckbox(
+    modifier: Modifier = Modifier,
+    partOfSpeech: PartOfSpeech,
+    checked: Boolean,
+    onClick: (PartOfSpeech) -> Unit,
+) {
+    YuzuCheckboxOption(
+        modifier = modifier,
+        checked = checked,
+        onCheckedChange = { onClick(partOfSpeech) },
+    ) {
+        HeaderWithSubtitle(
+            header = headerForPartOfSpeech(partOfSpeech),
+            subtitle = subtitleForPartOfSpeech(partOfSpeech),
+        )
+    }
+}
+
+@Composable
+private fun headerForPartOfSpeech(partOfSpeech: PartOfSpeech) = when (partOfSpeech) {
+    PartOfSpeech.Noun -> "Nouns"
+    PartOfSpeech.Verb -> "Verbs"
+    PartOfSpeech.Adjective -> "Adjectives"
+}
+
+@Composable
+private fun subtitleForPartOfSpeech(partOfSpeech: PartOfSpeech) = when (partOfSpeech) {
+    PartOfSpeech.Verb -> "Verb in infinite form"
+    else -> null
+}
+
+@PreviewDynamicColors
+@PreviewLightDark
+@Composable
+private fun PartsOfSpeechOptionsPreview() {
+    YuzuPreview {
+        PartsOfSpeechOptions(
+            modifier = Modifier.padding(4.dp),
+            selectedPartsOfSpeech = setOf(PartOfSpeech.Noun, PartOfSpeech.Verb, PartOfSpeech.Adjective),
+            onClick = {},
+        )
+    }
+}
---
+++ feature/wordgenerator/src/main/java/uvis/irin/feature/wordgenerator/ui/model/PartOfSpeech.kt
@@ -0,0 +1,7 @@
+package uvis.irin.feature.wordgenerator.ui.model
+
+enum class PartOfSpeech {
+    Noun,
+    Verb,
+    Adjective,
+}
